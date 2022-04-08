c5c26357fac6d9a8be9c436c2ac0c5c13d89ce80
joshua <zoniqhedgehog@gmail.com>
2022-04-08 13:50:49 +0200

    Initial commit

---
+++ src/me/spyro/testing/Main.java
@@ -0,0 +1,15 @@
+package me.spyro.testing;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        this.getCommand("example").setExecutor(new ExampleCommand(this));
+    }
+}
