deaab2fb5a40f9f40c22e1f52f8d28532458f35e
joshua <zoniqhedgehog@gmail.com>
2022-04-08 16:37:33 +0200

    get events

--- src/me/spyro/testing/Main.java
+++ src/me/spyro/testing/Main.java
@@ -1,15 +1,19 @@
 package me.spyro.testing;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Bukkit;
 
 public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
-
+        this.getCommand("example").setExecutor(new ExampleCommand(this));
+        Bukkit.getPluginManager().registerEvents(new Join(this),this);
+        Bukkit.getPluginManager().registerEvents(new Leave(this),this);
+        this.saveDefaultConfig();
     }
 
     @Override
     public void onDisable() {
-        this.getCommand("example").setExecutor(new ExampleCommand(this));
     }
 }
